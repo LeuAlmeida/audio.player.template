f009abcb9880b158675ca8efaa0683a8515f831d
Léu Almeida <leo@webid.net.br>
2019-10-11 16:48:43 -0300

    Data updates

--- data.js
+++ data.js
@@ -1,21 +1,20 @@
 window.audios = [
   {
-    title:
-      "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
-    artist: "Diego Fernandes",
-    cover: "como-comecei.jpg",
-    file: "como-comecei.mp3"
+    title: "Lorem ipsum dolor sit amet (1)",
+    artist: "Léu Almeida",
+    cover: "cover1.jpg",
+    file: "music1.mp3",
   },
   {
-    title: "5 dicas para uma carreira sólida como programador",
-    artist: "Diego Fernandes",
-    cover: "5-dicas-para-uma-carreira-solida-como-programador.jpg",
-    file: "5-dicas-para-uma-carreira-solida-como-programador.mp3"
+    title: "Lorem ipsum dolor sit amet (2)",
+    artist: "Léu Almeida",
+    cover: "cover2.jpg",
+    file: "music2.mp3",
   },
   {
-    title: "Júnior Pleno ou Sênior, qual a diferença?",
-    artist: "Diego Fernandes",
-    cover: "junior-pleno-ou-senior-qual-a-diferenca.jpg",
-    file: "junior-pleno-ou-senior-qual-a-diferenca.mp3"
+    title: "Lorem ipsum dolor sit amet (3)",
+    artist: "Léu Almeida",
+    cover: "cover3.jpg",
+    file: "music3.wav",
   }
 ];
