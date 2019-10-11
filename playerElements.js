763f4691c9d0a1fc408825ab2d7aa897f9dd8f20
Léu Almeida <leo@webid.net.br>
2019-10-11 18:34:46 -0300

    Create audio element

--- playerElements.js
+++ playerElements.js
@@ -3,6 +3,8 @@ export default {
     this.cover = document.querySelector(".card-image");
     this.title = document.querySelector(".card-content h5");
     this.artist = document.querySelector(".artist");
-    this.audio = document.querySelector("audio");
+  },
+  createAudioElement(audio) {
+    this.audio = new Audio(audio);
   }
 }
