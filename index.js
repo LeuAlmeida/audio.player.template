e7b6909607005d6e2254813dfb4fcda27c4bb587
Léu Almeida <leo@webid.net.br>
2019-10-12 17:55:31 -0300

    Code refactoring

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const cover = document.querySelector(".card-image");
-const title = document.querySelector(".card-content h5");
-const artist = document.querySelector(".artist");
-const audio = document.querySelector("audio");
-
-const data = {
-  title: "Lorem ipsum dolor sit amet (1)",
-  artist: "Léu Almeida",
-  cover: "files/cover1.jpg",
-  file: "files/music1.mp3"
-};
-
-cover.style.background = `url('${data.cover}') no-repeat center center / cover`;
-title.innerText = data.title;
-artist.innerText = data.artist;
-audio.src = data.file;
+window.addEventListener("load", player.start());
