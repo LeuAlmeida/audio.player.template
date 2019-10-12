6485ceb544336429167d1141700bfe9d052df970
Léu Almeida <leo@webid.net.br>
2019-10-12 17:45:57 -0300

    Data updates

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
-export default [
+window.audios = [
   {
     title: "Lorem ipsum dolor sit amet (1)",
     artist: "Léu Almeida",
     cover: "cover1.jpg",
-    file: "music1.mp3",
+    file: "music1.mp3"
   },
   {
     title: "Lorem ipsum dolor sit amet (2)",
     artist: "Léu Almeida",
     cover: "cover2.jpg",
-    file: "music2.mp3",
+    file: "music2.mp3"
   },
   {
     title: "Lorem ipsum dolor sit amet (3)",
     artist: "Léu Almeida",
     cover: "cover3.jpg",
-    file: "music3.mp3",
+    file: "music3.mp3"
   }
 ];
